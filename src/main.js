deca68c95ce4584bea0110a49ab5eabeb6edef06
Justo Bendezu <justocarlos.bc@gmail.com>
2020-08-10 00:17:32 -0500

    Mejorar la UI con filtros

--- src/main.js
+++ src/main.js
@@ -3,10 +3,13 @@ import App from './App.vue'
 import '@/assets/css/tailwind.css'
 
 import router from '@/router'
+import { dollarFilter, percentFilter } from '@/filters'
 
+Vue.filter('dollar', dollarFilter)
+Vue.filter('percent', percentFilter)
 Vue.config.productionTip = false
 
 new Vue({
-  router: router,
+  router,
   render: h => h(App)
 }).$mount('#app')
