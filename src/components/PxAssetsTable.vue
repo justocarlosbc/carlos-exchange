ed515c3cbaab46c3597f0d939009a19c13942609
Justo Bendezu <justocarlos.bc@gmail.com>
2020-08-10 16:45:22 -0500

    Mejorar proyecto: filtros y ordenar.

--- src/components/PxAssetsTable.vue
+++ src/components/PxAssetsTable.vue
@@ -121,7 +121,7 @@ export default {
       this.$router.push({ name: 'coin-detail', params: { id } })
     },
 
-    changeSortOrder(){
+    changeSortOrder() {
       this.sortOrder = this.sortOrder == 1 ? -1 : 1
     }
   }
